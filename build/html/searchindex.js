afc7c35e0e5c0c3b828646b1f8a23e4e997b1f45
Pavel Salnikov <flitvious@users.noreply.github.com>
2018-08-31 13:09:27 +0300

    initial commit

---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Sphinx build example\u2019s documentation!"],titleterms:{build:0,document:0,exampl:0,indic:0,sphinx:0,tabl:0,welcom:0}})
